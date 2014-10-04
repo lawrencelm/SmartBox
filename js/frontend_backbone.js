d20cc0f68a9d452d3fd3735f538035cfc6ff189f
Joshua Pham <joshua.pham@gmail.com>
2014-10-04 04:42:29 -0700

    begin modal login

--- js/frontend_backbone.js
+++ js/frontend_backbone.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
     $('body').keypress(function(e) {
         if (e.keyCode === 13) {
             var query = $('#searchinput').val();
-            $('#navigationText>a').append('<span id="query">' + query + '</span>');
+            /*$('#navigationText>a').append('<span id="query">' + query + '</span>');*/
             $('.searchLanding, #navigation, .searchbox').addClass('submitted');
+            /*$('#searchPrefix').css({'display': 'inline-block'});*/
         }
+    });
+    $('#login-button').click(function() {
+        $('#login-modal').removeClass('hidden');
     });
 });
 
