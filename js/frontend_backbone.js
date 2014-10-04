9c50c8de257ac46e93e9738b77bc9b70b7cd1676
Joshua Pham <joshua.pham@gmail.com>
2014-10-04 02:05:47 -0700

    restructured frontend js, added backbone

---
+++ js/frontend_backbone.js
@@ -0,0 +1,11 @@
+//pure jQuery part
+$(document).ready(function() {
+    $('body').keypress(function(e) {
+        if (e.keyCode === 13) {
+            var query = $('#searchinput').val();
+            $('#navigationText>a').append('<span id="query">' + query + '</span>');
+            $('.searchLanding, #navigation, .searchbox').addClass('submitted');
+        }
+    });
+});
+
