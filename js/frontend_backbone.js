93a7c45ac73db6609ba1dafec22a7f3af90d1e0a
Joshua Pham <joshua.pham@gmail.com>
2014-10-04 08:46:26 -0700

    some progress on frontend_backbone and started on Parse integration with modal

--- js/frontend_backbone.js
+++ js/frontend_backbone.js
@@ -1,4 +1,4 @@
-//pure jQuery part
+//pure jQuery part: SEARCH///
 $(document).ready(function() {
     $('body').keypress(function(e) {
         if (e.keyCode === 13) {
@@ -11,12 +11,63 @@ $(document).ready(function() {
     $('#login-button').click(function() {
         $('#login-modal').removeClass('hidden');
     });
+
 });
 
-var result_item = Backbone.Model.extend({
-    tagName: "div",
-    className: "result_item",
-    render: function() {
-        
+Parse.initialize('Xbx1p6suP8Fd2FjL2qr98AktVB5WfV5fRom9a1Dy', 'Q5Br45nLQaq08jtkpoIix1C1DheI9d6wV7SqoFNv');
+
+//LOGIN//
+$('#submitLogin').click(function() {
+    register();
+});
+
+var register = function() {
+    var user = new Parse.User();
+    //var user = new Parse.User();
+    /*var inputUsername = $('#inputUsername').val();
+    var inputPassword = $('#inputPassword').val();
+    var inputEmail = $('#inputEmail').val();*/
+    alert();
+    user.set('username', 'asdf');
+    user.set('password', 'asdfasdfasdf');
+    user.set('email', 'asdasdf@asdfasdf.com');
+    console.log(user);
+    user.signUp(null, {
+        success: function(user) {
+        // Hooray! Let them use the app now.
+        },
+        error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
+
+var Results = Backbone.Collection.extend({
+
+    model: Result_Item,
+});
+
+var Result_Item = Backbone.Model.extend({
+    initialize: function() {
+
     }
 });
+
+var Result_Item_View = Backbone.View.extend({
+    tagName: "div",
+    className: "result_item",
+    initialize: function() {
+        this.template = _.template($('#result_item').html());
+        /*$('#result_item').css({'display': 'inline-block'});*/
+    },
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    }
+});
+
+for (var i = 0; i < 10; i++) {
+    var view = new Result_Item_View();
+    $('#resultContainer').append(view.render().$el);
+}
